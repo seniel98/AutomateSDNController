39ec32335ee503eaf3f0f2b232088e917d47035f
Jose Daniel <jdpadron98@gmail.com>
2020-07-05 20:34:45 +0100

    Almost finished the automation SDN controller file

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ param = '-n' if platform.system().lower() == 'windows' else '-c'
 
 
 def set_controller(controller_ip):
+    """Open the serial ports and
+    write the commands to set the controller
+    in the Zodiac Fx SDN switch
+     """
     try:
         # Open ports COM3 AND COM4
         com3 = serial.Serial("COM3", 9600)
@@ -27,30 +31,36 @@ def set_controller(controller_ip):
         return None
 
 
-def ping_automation_controller_1():
-    hostname = "10.0.0.20 "  # example
-
-    response = os.system("ping " + param + " 1 " + hostname)
-
-    # and then check the response...
-    if response == 0:
-        print(hostname, 'is up!')
+def is_controller_up(controller):
+    """Checks if the controller is up
+    by pinging it. If the response is ok returns true
+    if its not ok return false
+    """
+    response = os.system("ping " + param + " 1 " + controller)
+    # Check the response
+    if response == 0:  # Ping is successful
+        print(controller, 'is up!')
+        return True
     else:
-        print(hostname, 'is down!')
-        set_controller("10.0.0.30")
+        print(controller, 'is down!')
+        return False
 
 
-def ping_automation_controller_2():
-    hostname = "10.0.0.30 "  # example
-
-    response = os.system("ping " + param + " 1 " + hostname)
-
-    # and then check the response...
-    if response == 0:
-        print(hostname, 'is up!')
+def ping_automation_controller(controller_1, controller_2):
+    """Set the controller if it is up, by default if
+    both connections are ok the controller set is 10.0.0.20"""
+    if is_controller_up(controller_1):
+        set_controller(controller_1)
     else:
-        print(hostname, 'is down!')
-        set_controller("10.0.0.20")
+        set_controller(controller_2)
 
 
-ping_automation_controller_1()
+def main():
+    """Main function. Automates the process of pinging
+    and sleep for 5 seconds before doing the previous stuff again"""
+    while True:
+        ping_automation_controller("10.0.0.20", "10.0.0.30")
+        time.sleep(5)
+
+
+main()
